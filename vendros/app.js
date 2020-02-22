3be9a405b8d09c8fee5e6f965da627807246194b
huseyn <atayevh1@gmail.com>
2020-02-22 13:53:23 +0400

    new229

--- vendros/app.js
+++ vendros/app.js
@@ -1,8 +1,12 @@
-$(".mySlider").slick({
-    slidesToShow: 1,
-    slidesToScroll:1,
-    dots:true,
-})
+var hbar=document.querySelector(".hidden-bar")
+var menu=document.querySelector(".fmenu")
+hbar.onclick=function(){
+    menu.classList.toggle("active")
+}
 
 
-
+var fli=document.querySelector(".fli")
+var phmenu=document.querySelector(".phonemenu")
+fli.onclick=function(){
+    phmenu.classList.toggle("active")
+}
